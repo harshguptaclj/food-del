cdf2e461a453dbcfc452757f529e26dab8391f4a
Harsh Gupta <96175114+harshguptaclj@users.noreply.github.com>
2024-07-04 17:25:25 +0530

    third-commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import validator from "validator"
 
 // login user
