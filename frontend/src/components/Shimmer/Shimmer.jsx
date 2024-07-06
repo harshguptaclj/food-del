21d75c7c29067f1c373b982433109d1c8df08625
Harsh Gupta <96175114+harshguptaclj@users.noreply.github.com>
2024-07-06 22:09:28 +0530

    added shimmer message

--- frontend/src/components/Shimmer/Shimmer.jsx
+++ frontend/src/components/Shimmer/Shimmer.jsx
@@ -4,6 +4,10 @@ import './Shimmer.css'
 const Shimmer = () => {
   return (
     <div className="bodi">
+      <div className="cards">
+      <div className="shimmer"></div>
+      <h3 className='text'>Notice: Kindly reload or reopen the website, if food items are not loaded even after 10 seconds.</h3>
+      </div>
       {Array.from({ length: 32 }).map((el, i) => {
         return (
           <div key={i} className="card">
